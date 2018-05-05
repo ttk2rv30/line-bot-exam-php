97fbc2e8423321ab4559e36ce73e40fe254cca23
ttk2rv30 <ttk2rv30@gmail.com>
2018-05-05 15:28:26 +0700

    Create bot_settings.php

---
+++ bot_settings.php
@@ -0,0 +1,7 @@
+<?php
+/// การตั้งค่าเกี่ยวกับ bot ใน LINE Messaging API
+define('LINE_MESSAGE_CHANNEL_ID','1576361851');
+define('LINE_MESSAGE_CHANNEL_SECRET','98574869965c1de653b34e4411efbd97
+');
+define('LINE_MESSAGE_ACCESS_TOKEN','PLXhyaxl5hdQ/EDzycfAmvv5RdkrX18IWhYMVl9zDVuaJNy84HbAe2wCREG/voLe1OR51zliFQPlgTR/F2qJkrDU+a9OJ5zMkTjwfGdozMtyaz7G+teEosY9BAb7jIGFqAmfob1wSWTmHkzVOje2/QdB04t89/1O/w1cDnyilFU=');
+?>
