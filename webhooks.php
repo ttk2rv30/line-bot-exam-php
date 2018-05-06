818bcdf190c04d35f36a61af64995b7467c4f369
ttk2rv30 <ttk2rv30@gmail.com>
2018-05-06 22:32:05 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -25,25 +25,7 @@ if (!is_null($events['events'])) {
 				'type' => 'text',
 				'text' => $text
 			];
-//---------------------------------------------
-$url = 'https://api.line.me/v2/bot/profile/'.$userId;
-			$data = [
-				'replyToken' => $replyToken,
-				'messages' => [$messages],
-			];
-			$post = json_encode($data);
-$headers = array('Authorization: Bearer ' . $access_token);
-$ch = curl_init($url);
-			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-			curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
-			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-			$result = curl_exec($ch);
-			curl_close($ch);
 
-			echo $result . $tmp "\r\n";
-//------------------------------------------
 			
 			
 			
