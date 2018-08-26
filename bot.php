28a3d62be9a6d6f5690017416768adefe0c3aa04
ttk2rv30 <ttk2rv30@gmail.com>
2018-08-27 02:38:06 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -290,7 +290,7 @@ $response = $bot->replyMessage($replyToken,$replyData);
 if ($response->isSucceeded()) {
 $url = 'https://www.lomrak.com/api.php'; 
   
-   $param1 = "User_ID=".$regID."User_Name=".$regName."User_Pic=".$regPic."User_Status".$regStatus;
+   $param1 = "User_ID=".$regID."User_Name=".$regName."User_Pic=".$regPic."User_Status=".$regStatus;
   
   /*$data = array(
         'fn' => "login" 
