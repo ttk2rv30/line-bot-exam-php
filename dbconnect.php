3657c38425dcdffb5f29d87a863b3042a4ca414b
ttk2rv30 <ttk2rv30@gmail.com>
2018-08-26 23:33:34 +0700

    Update dbconnect.php

--- dbconnect.php
+++ dbconnect.php
@@ -1,9 +1,10 @@
 <?php 
-$host ="203.146.127.100";//ชื่อโฮส
-$user ="lomrak_lpg";//ชื่อผู้ใช้
-$pass ="AG_LPG";//รหัสผ่าน
-$db ="lomrak_lpg";//ชื่อฐานข้อมูลที่เราสร้างไว้
-mysql_connect ($host,$user,$pass) or die ("ติดต่อเครื่อง Database Server ไม่สำเร็จ");//ติดต่อ Database Server
-mysql_select_db($db) or die ("ติดต่อฐานข้อมูลไม่สำเร็จ");//ติอต่อฐานข้อมูล
+$response = $bot->replyMessage($replyToken,$replyData);
+$host = "203.146.127.100";
+$user = "lomrak_lpg";
+$passwd = "AG_LPG";
+$dbname = "lomrak_lpg";
+mysql_connect($host, $user, $passwd) or die ("not connect");
+mysql_select_db($dbname) or die ("no table");
 mysql_query("SET NAMES utf8")//ตั้งค่าภาษา
 ?>
