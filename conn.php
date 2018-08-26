50106be1bc1f28aa0e58a25fbc3f419e27dc35c5
ttk2rv30 <ttk2rv30@gmail.com>
2018-08-26 23:07:47 +0700

    Create conn.php

---
+++ conn.php
@@ -0,0 +1,9 @@
+<?php 
+$host ="203.146.1277.100";//ชื่อโฮส
+$user ="lomrak_lpg";//ชื่อผู้ใช้
+$pass ="AG_LPG";//รหัสผ่าน
+$db ="lomrak_lpg";//ชื่อฐานข้อมูลที่เราสร้างไว้
+mysql_connect ($host,$user,$pass) or die ("ติดต่อเครื่อง Database Server ไม่สำเร็จ");//ติดต่อ Database Server
+mysql_select_db($db) or die ("ติดต่อฐานข้อมูลไม่สำเร็จ");//ติอต่อฐานข้อมูล
+mysql_query("SET NAMES utf8")//ตั้งค่าภาษา
+?>
