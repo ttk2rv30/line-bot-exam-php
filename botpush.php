2fd5967b9aa6a22381fd2309de4f03d19694ab59
ttk2rv30 <ttk2rv30@gmail.com>
2019-01-03 23:28:59 +0700

    Update botpush.php

--- botpush.php
+++ botpush.php
@@ -74,7 +74,7 @@ case 14:
 
 }
 
-$sms = "[Auto] สวัสดีค่ะ ".$card_name." ได้เข้ามาใช้บริการไปเมื่อวันที่ ". $pos_date ." เวลา " . $pos_time. " เป็นจำนวน ".$pos_total. " บาท ยอดเครดิตปัจจุบันคือ ".$current ." บาท "; 
+$sms = "สวัสดีค่ะ ".$card_name." ได้เข้ามาใช้บริการไปเมื่อวันที่ ". $pos_date ." เวลา " . $pos_time. " เป็นจำนวน ".$pos_total. " บาท ยอดเครดิตปัจจุบันคือ ".$current ." บาท "; 
 $sms = $sms . "*** กำลังใจจาก LPG ค่ะ *** " . $g_txt;
 $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($sms);
 $response = $bot->pushMessage($pushID, $textMessageBuilder);
